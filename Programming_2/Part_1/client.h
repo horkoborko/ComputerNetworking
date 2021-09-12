7df741bd4ff2466cc5629a33d7473bd3e024af18
Ari J <57876679+cloudandr0id@users.noreply.github.com>
2021-09-12 12:28:13 -0700

    Update client.h

--- Programming_2/Part_1/client.h
+++ Programming_2/Part_1/client.h
@@ -1,4 +1,25 @@
 /************************************************************************
  * libraries
  ************************************************************************/
-#include "server.h"
+// should always be there ...
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// socket/bind/listen/accept
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+
+// read/write/close
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/************************************************************************
+ * preprocessor directives
+ ************************************************************************/
+#define SERVER_ADDR "time.nist.gov" // time server ip
+#define PORT 13         // port the server will listen on
