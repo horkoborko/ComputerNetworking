b63f2055f5c7b122dc2fa55c4546bcca27580769
YenClutter <cklclutter@mail.com>
2021-09-13 19:18:19 -0700

    Added preliminary threading

--- Programming_2/Part_2/EchoServer.java
+++ Programming_2/Part_2/EchoServer.java
@@ -10,94 +10,95 @@ import java.util.logging.Logger;
 
 public class EchoServer {
 
-    private static ServerSocket serverSocket;
-    private static int port;
-    Socket clientSocket = null;
+  private static ServerSocket serverSocket;
+  private static int port;
+  Socket clientSocket = null;
 
-    public EchoServer(int port) {
-        try {
-            EchoServer.port = port;
-            EchoServer.serverSocket = new ServerSocket(port);
-        } catch (IOException ex) {
-            Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println("Error starting server on port " + port);
-            System.exit(1);
-        }
-
-        EchoServer.port = port;
+  public EchoServer(int port) {
+    try {
+      EchoServer.port = port;
+      EchoServer.serverSocket = new ServerSocket(port);
+    } catch (IOException ex) {
+      Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
+      System.err.println("Error starting server on port " + port);
+      System.exit(1);
     }
 
-    public void runServerLoop() throws IOException {
-        System.out.println("Echo server started");
+    EchoServer.port = port;
+  }
 
-        while (true) {
-            System.out.println("Waiting for connections on port #" + port);
+  public void runServerLoop() throws IOException {
+    System.out.println("Echo server started");
 
-            EchoThread passOver = new EchoThread();
+    while (true) {
+      System.out.println("Waiting for connections on port #" + port);
 
-            passOver.handleClient(serverSocket.accept());
-        }
+      EchoThread passOver = new EchoThread();
 
+      passOver.handleClient(serverSocket.accept());
     }
 
-    public void handleClient(Socket clientSocket) {
+  }
 
-        DataInputStream fromClient = null;
-        DataOutputStream toClient = null;
+  public void handleClient(Socket clientSocket) {
 
-        int charFromClient = 0;
-        int state = 0;
-        boolean keepGoing = true;
+    DataInputStream fromClient = null;
+    DataOutputStream toClient = null;
 
-        // show that we are connected to client
-        System.out.println("Client connected ...");
+    int charFromClient = 0;
+    int state = 0;
+    boolean keepGoing = true;
 
-        // first get the streams
-        try {
-            fromClient = new DataInputStream(clientSocket.getInputStream());
-            toClient = new DataOutputStream(clientSocket.getOutputStream());
-        } catch (IOException e) {
-            System.err.println("Error opening network streams");
-            return;
-        }
+    // show that we are connected to client
+    System.out.println("Client connected ...");
 
-        // now talk to the client
-        while (keepGoing) {
-            try {
-                charFromClient = fromClient.readByte();
-                System.out.print((char)charFromClient);
-            } catch (IOException e) {
-                System.err.println("Error reading character from client");
-                return;
-            }
-
-            try {
-                toClient.writeByte(charFromClient);
-            } catch (IOException e) {
-                System.err.println("Error writing character to client");
-                return;            }
-
-            if (charFromClient == 'q') {
-                System.out.println("\nBailing out!");
-                keepGoing = false;
-            }
-        }
-
-        try {
-            clientSocket.close();
-        } catch (IOException e) {
-            System.err.println("Error closing socket to client");
-        }
-
+    // first get the streams
+    try {
+      fromClient = new DataInputStream(clientSocket.getInputStream());
+      toClient = new DataOutputStream(clientSocket.getOutputStream());
+    } catch (IOException e) {
+      System.err.println("Error opening network streams");
+      return;
     }
 
+    // now talk to the client
+    while (keepGoing) {
+      try {
+        charFromClient = fromClient.readByte();
+        System.out.print((char) charFromClient);
+      } catch (IOException e) {
+        System.err.println("Error reading character from client");
+        return;
+      }
 
-    public static void main(String args[]) throws Exception {
-        // create instance of echo server
-        // note that hardcoding the port is bad, here we do it just for simplicity reasons
-        EchoServer echoServer = new EchoServer(23657);
+      try {
+        toClient.writeByte(charFromClient);
+      } catch (IOException e) {
+        System.err.println("Error writing character to client");
+        return;
+      }
 
-        // fire up server loop
-        echoServer.runServerLoop();
+      if (charFromClient == 'q') {
+        System.out.println("\nBailing out!");
+        keepGoing = false;
+      }
     }
+
+    try {
+      clientSocket.close();
+    } catch (IOException e) {
+      System.err.println("Error closing socket to client");
+    }
+
+  }
+
+  public static void main(String args[]) throws Exception {
+    // create instance of echo server
+    // note that hardcoding the port is bad, here we do it just for simplicity
+    // reasons
+    EchoServer echoServer = new EchoServer(23657);
+
+    // fire up server loop
+    echoServer.runServerLoop();
+  }
 }
--- Programming_2/Part_2/server.c
+++ Programming_2/Part_2/server.c
@@ -11,40 +11,48 @@ int main(int argc, char** argv) {
 
     // sent when ,client disconnected
     signal(SIGPIPE, SIG_IGN);
-    
+
     // create unnamed network socket for server to listen on
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
-    
+
     // name the socket (making sure the correct network byte ordering is observed)
     server_address.sin_family      = AF_INET;           // accept IP addresses
     server_address.sin_addr.s_addr = htonl(INADDR_ANY); // accept clients on any interface
     server_address.sin_port        = htons(PORT);       // port to listen on
-    
+
     // binding unnamed socket to a particular port
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
         perror("Error binding socket");
         exit(EXIT_FAILURE);
     }
-    
+
     // listen for client connections (pending connections get put into a queue)
     if (listen(server_socket, NUM_CONNECTIONS) == -1) {
         perror("Error listening on socket");
         exit(EXIT_FAILURE);
     }
-    
+
     // server loop
-    while (TRUE) {
-        
+    while (true)
+    {
+
         // accept connection to client
-        if ((client_socket = accept(server_socket, NULL, NULL)) == -1) {
+        //create new thread for every incoming client
+        if ((client_socket = pthread_create( accept(server_socket, NULL, NULL)) == -1) )
+        {
             perror("Error accepting client");
-        } else {
+        }
+        else
+        {
             printf("\nAccepted client\n");
             handle_client(client_socket);
         }
+
+        // detach tread
+        pthread_detatch(server_socket);
     }
 }
 
@@ -55,23 +63,24 @@ int main(int argc, char** argv) {
 
 void handle_client(int client_socket) {
     char input;
-    int keep_going = TRUE;
-    
-    while (keep_going) {
+    int keep_going = true;
+
+    while (keep_going)
+    {
         // read char from client
         switch (read(client_socket, &input, sizeof(char))) {
             case 0:
-                keep_going = FALSE;
+                keep_going = false;
                 printf("\nEnd of stream, returning ...\n");
                 break;
             case -1:
                 perror("Error reading from network!\n");
-                keep_going = FALSE;
+                keep_going = false;
                 break;
         }
-   
+
         printf("%c", input);
-        
+
         // check if we terminate
         if (input == 'q') {
             keep_going = FALSE;
@@ -81,7 +90,7 @@ void handle_client(int client_socket) {
         write(client_socket, &input, sizeof(char));
 
     }
-    
+
     // cleanup
     if (close(client_socket) == -1) {
         perror("Error closing socket");
