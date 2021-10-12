728b20130883ec744c5a6e2186d8115a955bfcee
Ari J <57876679+cloudandr0id@users.noreply.github.com>
2021-10-11 19:12:16 -0700

    starting assignment 5

---
+++ assignment_5/clientMake.mk
@@ -0,0 +1,18 @@
+CC = gcc
+DEBUG = -g
+CFLAGS = -Wall -std=c99 -pedantic -pthread -c  $(DEBUG)
+LFLAGS = -Wall -std=c99 -pedantic -pthread $(DEBUG)
+
+client : threadpool_client.c threadpool.o
+	$(CC) $(LFLAGS) threadpool_client.c threadpool.o -o client
+
+server : multiThreadServer.c
+	$(CC) $(LFLAGS) multiThreadServer.c -o server
+
+threadpool.o : threadpool.c
+	$(CC) $(CFLAGS) threadpool.c
+
+
+
+clean:
+	\rm *.o client
---
+++ assignment_5/threadpool_client.h
@@ -0,0 +1,34 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+
+// turn on debugging
+#define DBG
+#include "dbg.h"
+
+int three_a_plus_one(int input);
+int three_a_plus_one_rec(int input);
+void three_a_plus_one_wrapper(void *number_ptr);
+
+/************************************************************************
+ * preprocessor directives
+ ************************************************************************/
+#define SERVER_ADDR "127.0.0.1" // loopback ip address
+#define PORT 23658              // port the server will listen on
+
+// number of pending connections in the connection queue
+#define NUM_CONNECTIONS 1
+
+#define NUMBER_TASKS 100000
+
+#define FALSE 0
+#define TRUE !FALSE
