296355f50ada02cf54533d6b4c22ded9f879a4a6
Ari J <57876679+cloudandr0id@users.noreply.github.com>
2021-10-12 19:18:31 -0700

    added make files

--- assignment_5/serverMake.mk
+++ assignment_5/serverMake.mk
@@ -3,8 +3,8 @@ DEBUG = -g
 CFLAGS = -Wall -std=c99 -pedantic -pthread -c  $(DEBUG)
 LFLAGS = -Wall -std=c99 -pedantic -pthread $(DEBUG)
 
-client : threadpool_client.c threadpool.o
-	$(CC) $(LFLAGS) threadpool_client.c threadpool.o -o client
+server : t_server.c threadpool.o
+	$(CC) $(LFLAGS) t_server.c threadpool.o -o tserver
 
 threadpool.o : threadpool.c
 	$(CC) $(CFLAGS) threadpool.c
